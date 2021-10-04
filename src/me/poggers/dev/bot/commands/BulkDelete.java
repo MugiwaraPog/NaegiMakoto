bd44eed3f02803fa471aa243ba50ea03fb5a3d91
MugiwaraPog <gamervicentinho@gmail.com>
2021-10-04 13:45:03 -0300

    deletando msgs em massa pt 2

--- src/me/poggers/dev/bot/commands/BulkDelete.java
+++ src/me/poggers/dev/bot/commands/BulkDelete.java
@@ -1,11 +1,17 @@
 package me.poggers.dev.bot.commands;
 
 import me.poggers.dev.bot.main.NaegiMakoto;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BulkDelete extends ListenerAdapter{
+
+    private final Map<String, Boolean> isDeletingAndCreatingChannel = new HashMap<>();
 
 
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
@@ -15,8 +21,33 @@ public class BulkDelete extends ListenerAdapter{
                 + "clear" )){
 
             event.getChannel().deleteMessages(event.getChannel().getHistory()
-                            .retrievePast(5).complete()).complete();
+                            .retrievePast(30).complete()).complete();
+        }
+
+        if(args[0].equalsIgnoreCase(NaegiMakoto.prefixMap.get(
+                event.getGuild().getId()) + "clearall")){
+
+            if(event.getMember().hasPermission(Permission.MANAGE_SERVER)){
+                event.getChannel().sendMessage("Voce não tem permissão para limpar este canal.")
+                        .queue();
+                return;
+            }
+
+
+            isDeletingAndCreatingChannel.put(event.getGuild().getId(), true);
+
 
         }
+        if(args[0].equalsIgnoreCase(NaegiMakoto.prefixMap.get(
+                event.getGuild().getId()) + "confirm")){
+            if(isDeletingAndCreatingChannel.get(event.getGuild().getId())){
+                event.getChannel().createCopy().queue();
+                event.getChannel().delete().queue();
+
+                isDeletingAndCreatingChannel.replace(event.getGuild().getId(), false);
+            }
+
+        }
+
     }
 }
